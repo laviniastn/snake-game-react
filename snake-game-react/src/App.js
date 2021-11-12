218cdc747e01bf2e4d9148d63e25045afe18fdae
laviniastn <laviniastn25@gmail.com>
2021-11-12 10:25:49 +0200

    snake

---
+++ snake-game-react/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, { Component } from 'react';
+import Snake from './Snake';
+import Food from './Food';
+
+class App extends Component {
+  
+  state = {
+    food:[6,8],
+    snakeDots: [
+      [0,0],
+      [2,0]
+      
+    ]
+  }
+
+  render(){
+  return (
+    <div className="game-area">
+      <Snake snakeDots={this.state.snakeDots}/>
+      <Food dot={this.state.food}/>
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ snake-game-react/src/Food.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { findDOMNode } from "react-dom";
+
+export default (props) => {
+   
+   const style = {
+       left: `${props.dot[0]}%`,
+       top: `${props.dot[1]}%`
+   }
+
+    return(
+        <dic className="snake-food" style={style}></dic> 
+    )
+}
